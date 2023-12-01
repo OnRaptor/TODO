604846d5ccc5b7c86c9ce259992277a7e7ba07b8
lightless <raptorahead@gmail.com>
2023-12-01 17:01:53 +0500

    Add project files.

---
+++ DB/Data/APIContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DB.Data
+{
+    public class APIContext : DbContext
+    {
+        public APIContext(DbContextOptions<APIContext> options) : base(options)
+        { }
+        public DbSet<User> Users { get; set; }
+        public DbSet<TODO> Tasks { get; set; }
+    }
+}
---
+++ DB/Models/TODO.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DB.Models
+{
+    public class TODO
+    {
+        public int Id {  get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        [DataType(DataType.DateTime)]
+        public DateTime Deadline {  get; set; }
+        public string Priority { get; set; }
+        public bool IsCompleted { get; set; }
+        public User Author { get; set; }
+    }
+}
